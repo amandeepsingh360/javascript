cf1fcc000d5afe571622c2aaa4b997610e5f1162
amandeepsingh360 <amandeepsingh_ee21b6_50@dtu.ac.in>
2023-10-01 21:37:18 +0530

    Datatypes

---
+++ datatype.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all JS code a newer version
+
+// alert cannot be used in node
+// alert("hello") // error
+
+let name = "aman"
+let age = 19;
+let isloggedIn = true
+/*
+Types of DataTypes:-
+1. number
+2. String(used in '' & "")
+3. Boolean
+4. null => standalone value(type is object)
+5. undefined => not given a value(type is undefined)
+6. Symbol => Used in React more(to identify uniqueness)
+7. Object
+*/
+
+// typeof(isLoggedIn) & typeof isLoggedIn are same
+
+console.log(typeof(null)); 
